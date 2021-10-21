fa4b9523a8bcde2696477956c17cac31ed826aca
Nikow <nicolas@ruche.me>
2021-10-21 15:26:54 +0200

    begin graham scan

---
+++ Maths/Assets/___PROJET/Scripts/Convex Hull 2D/GrahamScan.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ESGI.ConvexHull2D
+{
+    public class GrahamScan : MonoBehaviour
+    {
+        public List<Vector2> positions;
+
+
+    }
+}
