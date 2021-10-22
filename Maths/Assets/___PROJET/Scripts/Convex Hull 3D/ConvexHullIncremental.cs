d3e9ca27b0c22b07caa4b142c04ed7e8ae67da37
Nikow <nicolas.ruche.entreprise@gmail.com>
2021-10-22 12:17:39 +0200

    chore: add some scripts and scenes to implement

---
+++ Maths/Assets/___PROJET/Scripts/Convex Hull 3D/ConvexHullIncremental.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ESGI.ConvexHull3D
+{
+    public class ConvexHullIncremental : MonoBehaviour
+    {
+        public void Reset()
+        {
+        }
+        
+        public void Awake()
+        {
+        }
+        
+        public void Start()
+        {
+        }
+        
+        public void OnEnable()
+        {
+        }
+        
+        public void OnDisable()
+        {
+        }
+        
+        public void Update()
+        {
+        }
+        
+        public void OnDestroy()
+        {
+        }
+    }
+}
---
+++ Maths/Assets/___PROJET/Scripts/Voronoi/VoronoiDual.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ESGI.Voronoi
+{
+    public class VoronoiDual : MonoBehaviour
+    {
+        public void Reset()
+        {
+        }
+        
+        public void Awake()
+        {
+        }
+        
+        public void Start()
+        {
+        }
+        
+        public void OnEnable()
+        {
+        }
+        
+        public void OnDisable()
+        {
+        }
+        
+        public void Update()
+        {
+        }
+        
+        public void OnDestroy()
+        {
+        }
+    }
+}
