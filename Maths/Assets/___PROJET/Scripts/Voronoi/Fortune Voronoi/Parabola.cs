7704c906d5b1f0e11634048ecc6b5f5b7f1665ce
Nikow <nicolas.ruche.entreprise@gmail.com>
2021-10-22 14:48:04 +0200

    feat: can compute parabola from focus and directrix

---
+++ Maths/Assets/___PROJET/Scripts/Voronoi/Fortune Voronoi/Parabola.cs
@@ -0,0 +1,47 @@
+﻿using PGSauce.Core.PGDebugging;
+using UnityEngine;
+
+namespace ESGI.Common
+{
+    public class Parabola
+    {
+        private float _a;
+        private float _b;
+        private float _c;
+
+        public void ComputeParabolaFromFocusAndHorizontalLine(Vector2 focus, float lineY)
+        {
+            //https://www.varsitytutors.com/hotmath/hotmath_help/topics/finding-the-equation-of-a-parabola-given-focus-and-directrix
+            _a = focus.x;
+            _b = focus.y;
+            _c = lineY;
+            /*
+            var B = focus.y - lineY;
+            var A = focus.y * focus.y - lineY * lineY;
+
+            if (B == 0.0f)
+            {
+                _a = _b = _c = 0;
+                PGDebug.Message("Undefined Parabola").LogTodo();
+            }
+
+            _a = 1 / (2 * B);
+            _b = -focus.x / B;
+            _c = (1 + A) / (2 * B);*/
+        }
+
+        public float Compute(float x)
+        {
+            var A = (x - _a) * (x - _a);
+            var B = _b * _b - _c * _c;
+            var C = 2 * (_b - _c);
+            if (C == 0.0f)
+            {
+                PGDebug.Message("Undefined Parabola").LogTodo();
+                return 0f;
+            }
+
+            return (A + B) / C;
+        }
+    }
+}
---
+++ Maths/Assets/___PROJET/Scripts/Voronoi/Fortune Voronoi/ParabolaTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using PGSauce.Core;
+using PGSauce.Core.Utilities;
+using Shapes;
+using UnityEngine;
+
+namespace ESGI.Common
+{
+    public class ParabolaTest : ImmediateModeShapeDrawer
+    {
+        public Vector2 focus;
+        public float lineY = -1;
+        public MinMax<float> xRange;
+        [Min(2)] public int iterations = 50;
+
+        private Parabola _parabola;
+
+        private void Awake()
+        {
+            _parabola = new Parabola();
+        }
+
+        public override void DrawShapes(Camera cam)
+        {
+            base.DrawShapes(cam);
+            using (Draw.Command(cam))
+            {
+                _parabola.ComputeParabolaFromFocusAndHorizontalLine(focus, lineY);
+                var step = (xRange.max - xRange.min) / (iterations - 1);
+                for (int i = 0; i < iterations; i++)
+                { 
+                    var x = xRange.min + step * i; 
+                    var y = _parabola.Compute(x); 
+                    Draw.Disc(new Vector2(x, y), 0.2f);
+                }
+
+                Draw.Color = PGColors.Greenish;
+                Draw.Disc(focus, 0.2f);
+                
+                Draw.Line(new Vector3(xRange.min, lineY), new Vector3(xRange.max, lineY));
+            }
+        }
+    }
+}
